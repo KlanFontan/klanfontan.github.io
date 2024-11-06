96efeab92c80bafb0f362ee82570913ee67e8f95
Jaysee <96378356+JAYSEECODES@users.noreply.github.com>
2024-11-06 20:57:30 +0300

    Fixes for 0.12

--- tele.js
+++ tele.js
@@ -22,3 +22,13 @@ function expandable() {
     }
 }
 
+function getTelegramUser() {
+    return tg.initDataUnsafe.user.username;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const playerTextElement = document.getElementById('player-text');
+    const telegramUser = getTelegramUser();
+    playerTextElement.textContent = telegramUser;
+});
+
